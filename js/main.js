2c9e3aead4e6fe5263388eaec5bc6f02dcad6ea9
Ahmed <ahmedfathiaboelanin@gmail.com>
2022-10-01 01:17:40 +0200

    new version

--- js/main.js
+++ js/main.js
@@ -18,27 +18,3 @@ let landingBtn = document.getElementById("landing-btn");
 landingBtn.onclick = () => {
   window.scrollTo(0, 650);
 };
-// owl carousel
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: true,
-    },
-    600: {
-      items: 1,
-      nav: false,
-    },
-    1000: {
-      items: 3,
-      nav: true,
-      loop: true,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      autoplayHoverPause: true,
-    },
-  },
-});
